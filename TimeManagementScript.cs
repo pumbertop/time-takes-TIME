ed7e862b430f93d0e9aeea424c74d9792d912588
pumbertop <pumbertop@gmail.com>
2015-07-04 14:58:56 -0400

    Revert "Revert "check collisions""
    This reverts commit b2e0b50a991e5b03e951dc922de5d985840429e7.

---
+++ TimeManagementScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System;
+
+public class TimeManagementScript : MonoBehaviour {
+
+	public TimeSpan currentTime;
+	public float currentSeconds;
+
+	void Awake() {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+
+	void Update () {
+		currentTime = DateTime.Now.TimeOfDay;
+		currentSeconds = (float)currentTime.TotalSeconds;
+	}
+}
